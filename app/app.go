7f86cb29cede2eb801bd4c0bff3805680c15220e
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2024-04-30 12:40:32 +0300

    tokenize the json string

--- app/app.go
+++ app/app.go
@@ -1,11 +1,23 @@
 package app
 
-import "regexp"
+import (
+	"regexp"
+)
 
 func App(readFile func(name string) ([]byte, error), args []string) (string, error) {
 	fileContentInByteArray, _ := readFile(args[1])
 	fileContentString := string(fileContentInByteArray)
-	regex := regexp.MustCompile(`(?s)\A\s*(\[\s*((\s*(null|true|false|-{0,1}\d+\.{0,1}\d+|-{0,1}\d+|"[^\s"]+"|\[[^][]*\]|{[^}{]*}|\[.*\[.*\].*\]|\{.*\{.*\}.*\}){1}\s*,\s*)*(\s*(null|true|false|-{0,1}\d+\.{0,1}\d+|-{0,1}\d+|"[^\s"]+"|\[[^][]*\]|{[^}{]*}|\[.*\[.*\].*\]|\{.*\{.*\}.*\}){1}\s*){1}){0,1}\]|{\s*((\s*"[^\s"]+"\s*:\s*(null|true|false|-{0,1}\d+\.{0,1}\d+|-{0,1}\d+|"[^\s"]+"|\[[^][]*\]|{[^}{]*}|\[.*\[.*\].*\]|\{.*\{.*\}.*\}){1}\s*,\s*)*(\s*"[^\s"]+"\s*:\s*(null|true|false|-{0,1}\d+\.{0,1}\d+|-{0,1}\d+|"[^\s"]+"|\[[^][]*\]|{[^}{]*}|\[.*\[.*\].*\]|\{.*\{.*\}.*\}){1}\s*){1}){0,1}}){1}\s*\z`)
+	numbers := `-{0,1}\d+\.{0,1}\d+|-{0,1}\d+`
+	inner_brackets := `\[[^][]*\]|{[^}{]*}|\[.*\[.*\].*\]|\{.*\{.*\}.*\}`
+	inner_element := `\s*(null|true|false|` + numbers + `|"[^\s"]+"|` + inner_brackets + `){1}`
+	last_element_in_outer_squrare_brackets := `(` + inner_element + `\s*)`
+	multiple_elments_in_outer_square_brackets := `(` + inner_element + `\s*,\s*)*`
+	outer_square_brackets := `\[\s*(` + multiple_elments_in_outer_square_brackets + last_element_in_outer_squrare_brackets + `{1}){0,1}\]`
+	object_key := `"[^\s"]+"\s*`
+	last_element_in_outer_curly_brackets := `(\s*` + object_key + `:` + inner_element + `\s*)`
+	multiple_elments_in_outer_curly_brackets := `(\s*` + object_key + `:` + inner_element + `\s*,\s*)*`
+	outer_curly_brakets := `{\s*(` + multiple_elments_in_outer_curly_brackets + last_element_in_outer_curly_brackets + `{1}){0,1}}`
+	regex := regexp.MustCompile(`(?s)\A\s*(` + outer_square_brackets + `|` + outer_curly_brakets + `){1}\s*\z`)
 	if !regex.MatchString(fileContentString) {
 		return "This is an invalid JSON", nil
 	}
